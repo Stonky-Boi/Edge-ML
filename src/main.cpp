22edf4be37ede63c92e9c8d91b3105e433472e4c
Stonky-Boi <arnav63366@gmail.com>
2025-08-15 10:35:44 +0530

    better output

--- src/main.cpp
+++ src/main.cpp
@@ -7,20 +7,22 @@
 #include <dense2_weights.h>
 #include <test_images.h>
 
-
-void setup() {
+void setup()
+{
   Serial.begin(115200);
-  while (!Serial);
+  while (!Serial)
+    ;
   Serial.println("Starting");
 
   NeuralNetwork nn(dense1_weights, dense1_bias, dense2_weights, dense2_bias);
 
-
   float count = 0;
   float num = 0;
-  for (int img = 1; img < 101; ++img) {
+  for (int img = 1; img < 101; ++img)
+  {
     std::vector<float> input(784);
-    for (int i = 0; i < 784; ++i) {
+    for (int i = 0; i < 784; ++i)
+    {
       input[i] = pgm_read_float(&test_images[img * 784 + i]);
     }
 
@@ -29,37 +31,55 @@ void setup() {
     Serial.print(":  ");
 
     std::vector<float> logits = nn.predict(input);
-    if (!logits.empty()) {
+    if (!logits.empty())
+    {
 
       int predicted = 0;
       float best = logits[0];
-      for (int j = 1; j < 10; ++j) {
-        if (logits[j] > best) {
+      for (int j = 1; j < 10; ++j)
+      {
+        if (logits[j] > best)
+        {
           best = logits[j];
           predicted = j;
         }
       }
 
+      const char *classNames[] = {
+          "T-shirt/top",
+          "Trouser",
+          "Pullover",
+          "Dress",
+          "Coat",
+          "Sandal",
+          "Shirt",
+          "Sneaker",
+          "Bag",
+          "Ankle boot"};
+
       Serial.print("Predicted class: ");
-      Serial.print(predicted);
+      Serial.print(classNames[predicted]);
       Serial.print(",  ");
-      Serial.print("Actual Class: ");
-      Serial.println(int(test_labels[img]));
+      Serial.print("Actual class: ");
+      Serial.println(classNames[int(test_labels[img])]);
       num++;
-      if (predicted==int(test_labels[img])){
+      if (predicted == int(test_labels[img]))
+      {
         count++;
       }
       delay(200);
-    } else {
+    }
+  
+    else
+    {
       Serial.println("Oops, image " + String(img) + " failed");
     }
-
   }
   Serial.print("Accuracy: ");
-  Serial.print((count/num)*100);
+  Serial.print((count / num) * 100);
   Serial.println(" %");
 }
 
-void loop() {
-
+void loop()
+{
 }
