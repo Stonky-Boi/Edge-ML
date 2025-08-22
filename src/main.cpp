55026fc745a5aad1b547668bb2bcd9a046dd4e67
Stonky-Boi <arnav63366@gmail.com>
2025-08-23 00:08:11 +0530

    expo fix

--- src/main.cpp
+++ src/main.cpp
@@ -7,22 +7,15 @@
 #include <dense2_weights.h>
 #include <test_images.h>
 
-void setup()
-{
-  Serial.begin(115200);
-  while (!Serial)
-    ;
-  Serial.println("Starting");
+NeuralNetwork nn(dense1_weights, dense1_bias, dense2_weights, dense2_bias);
 
-  NeuralNetwork nn(dense1_weights, dense1_bias, dense2_weights, dense2_bias);
-
+void runInference() {
   float count = 0;
   float num = 0;
-  for (int img = 1; img < 101; ++img)
-  {
+
+  for (int img = 1; img < 101; ++img) {
     std::vector<float> input(784);
-    for (int i = 0; i < 784; ++i)
-    {
+    for (int i = 0; i < 784; ++i) {
       input[i] = pgm_read_float(&test_images[img * 784 + i]);
     }
 
@@ -31,31 +24,20 @@ void setup()
     Serial.print(":  ");
 
     std::vector<float> logits = nn.predict(input);
-    if (!logits.empty())
-    {
-
+    if (!logits.empty()) {
       int predicted = 0;
       float best = logits[0];
-      for (int j = 1; j < 10; ++j)
-      {
-        if (logits[j] > best)
-        {
+      for (int j = 1; j < 10; ++j) {
+        if (logits[j] > best) {
           best = logits[j];
           predicted = j;
         }
       }
 
       const char *classNames[] = {
-          "T-shirt/top",
-          "Trouser",
-          "Pullover",
-          "Dress",
-          "Coat",
-          "Sandal",
-          "Shirt",
-          "Sneaker",
-          "Bag",
-          "Ankle boot"};
+        "T-shirt/top", "Trouser", "Pullover", "Dress", "Coat",
+        "Sandal", "Shirt", "Sneaker", "Bag", "Ankle boot"
+      };
 
       Serial.print("Predicted class: ");
       Serial.print(classNames[predicted]);
@@ -63,23 +45,35 @@ void setup()
       Serial.print("Actual class: ");
       Serial.println(classNames[int(test_labels[img])]);
       num++;
-      if (predicted == int(test_labels[img]))
-      {
+      if (predicted == int(test_labels[img])) {
         count++;
       }
       delay(200);
-    }
-  
-    else
-    {
+    } else {
       Serial.println("Oops, image " + String(img) + " failed");
     }
   }
+
   Serial.print("Accuracy: ");
   Serial.print((count / num) * 100);
   Serial.println(" %");
 }
 
-void loop()
-{
+void setup() {
+  Serial.begin(115200);
+  while (!Serial);
+  Serial.println("ESP Ready. Type 'start' to run inference.");
+}
+
+void loop() {
+  if (Serial.available()) {
+    String command = Serial.readStringUntil('\n');
+    command.trim();
+
+    if (command.equalsIgnoreCase("start")) {
+      Serial.println("Starting inference...");
+      runInference();
+      Serial.println("Done. Type 'start' again to rerun.");
+    }
+  }
 }
